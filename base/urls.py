30d85e6eb49518429c7a6f53a8f6e731b68fb9c5
Dmitrii Gherastovschi <dgherastovschi@icloud.com>
2023-06-23 18:55:23 +0500

    unit create update and delete functionality

--- base/urls.py
+++ base/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
-from .views import UnitList, UnitDetail
+from .views import UnitList, UnitDetail, UnitCreate, UnitUpdate, UnitDelete
 
 urlpatterns = [
     path('', UnitList.as_view(), name='units'),
     path('unit/<int:pk>/', UnitDetail.as_view(), name='unit'),
+    path('unit-create/', UnitCreate.as_view(), name='unit-create'),
+    path('unit-update/<int:pk>/', UnitUpdate.as_view(), name='unit-update'),
+    path('unit-delete/<int:pk>/', UnitDelete.as_view(), name='unit-delete'),
 ]
--- base/views.py
+++ base/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.urls import reverse_lazy
+
 from .models import Unit
 
 class UnitList(ListView):
@@ -9,3 +12,22 @@ class UnitList(ListView):
 
 class UnitDetail(DetailView):
     model = Unit
+    context_object_name = 'unit'
+    template_name = 'base/unit.html'
+
+class UnitCreate(CreateView):
+    model = Unit
+    fields = '__all__'
+    success_url = reverse_lazy('units')
+
+class UnitUpdate(UpdateView):
+    model = Unit
+    fields = '__all__'
+    success_url = reverse_lazy('units')
+
+class UnitDelete(DeleteView):
+    model = Unit
+    context_object_name = 'unit'
+    success_url = reverse_lazy('units')
+
+    
